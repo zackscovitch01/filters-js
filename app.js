68501a8577c29d4fae93912d9409488b5988b6e3
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-06 06:31:07 -0400

    displayProducts()

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('filters project');
+let filteredProducts = [...products];
+
+const productsContainer = document.querySelector(".products-container");
+
+const displayProducts = () => {
+  productsContainer.innerHTML = filteredProducts
+    .map(({ id, title, image, price }) => {
+      return `<article class="product" data-id="${id}">
+          <img
+            src="${image}"
+            alt=""
+            class="product-img img"
+          />
+          <footer>
+            <h5 class="product-name">${title}</h5>
+            <span class="product-price">$${price}</span>
+          </footer>
+        </article>`;
+    })
+    .join("");
+};
+
+displayProducts();
